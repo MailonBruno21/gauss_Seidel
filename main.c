c9d2a287a4df5401099a5d91027851a22e07f751
Mailon Bruno Pedri de Campos <45343777+MailonBruno21@users.noreply.github.com>
2019-11-23 23:25:55 -0400

    Metodo de Gauss-Seidel em C

--- main.c
+++ main.c
@@ -2,92 +2,99 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int criterio_Linhas(float x[100][100], int quant_Element){
-    float somatorio = 0;
+int criterio_Linhas(double x[100][100], int quant_Element){
+    double somatorio = 0;
     //REALIZA O SOMATORIO DAS POSIÇÕES DA MATRIZ COM "i" DE "0" AO FIM DA EQUAÇÃO, COM "i != j"
     for(int i = 0; i < quant_Element; i++){
         for(int j = 0; j < quant_Element; j++){
             if (i != j){
-            somatorio = x[i][j] + somatorio;
+                if( x[i][j] < 0){
+                    somatorio = (x[i][j] * -1) + somatorio;
+                }else{
+                    somatorio = x[i][j] + somatorio;
+                }
             }
         }
         //FAZ O MODULO CASO SOMATORIO SEJA NEGATIVO
-        if(somatorio < 0){
-            somatorio = somatorio * -1;
+
+        if((somatorio/x[i][i]) >= 1){
+            return -1;   //CRITÉRIO NÃO ATENDIDO RETORNA VALOR 1
         }
+
     }
-    if(somatorio < 1){
-        return 1;   //CRITÉRIO ATENDIDO RETORNA VALOR 1
-    }else{
-        return -1;  //CRITÉRIO NÃO ATENDIDO RETORNA VALOR -1
-    }
+    return 1;  //CRITÉRIO ATENDIDO RETORNA VALOR -1
 }
 
-int estri_Diag_Domin(float x[100][100], int quant_Element){
-
+int estri_Diag_Domin(double x[100][100], int quant_Element){
     //REALIZA O SOMATORIO DAS POSIÇÕES DA MATRIZ COM "i" DE "0" AO FIM DA EQUAÇÃO, COM "i != j"
     for(int i = 0; i < quant_Element; i++){
-        float somatorio = 0;
+        double somatorio = 0;
         for(int j = 0; j < quant_Element; j++){
             if (i != j){
-            somatorio = x[i][j] + somatorio;
-            }
-            if(x[i][i] < 0){
-                if(x[i][i]*-1 < somatorio){
-                    return -1; //INDICA QUE A MATRIZ NÃO É ESTRITAMENTE DIAGONAL DOMINANTE
-                }
-            }
-        }
-    }
-    return 1; //INDICA QUE A MATRIZ É ESTRITAMENTE DIAGONAL DOMINANTE
-}
-
-int criterio_Sassenfeld(float x[100][100], int quant_Element){
-
-    float b[100];
-    int aux = 0;
-    //REALIZA O SOMATORIO DAS POSIÇÕES DA MATRIZ COM "i" DE "0" AO FIM DA EQUAÇÃO, COM "i != j"
-
-    float somatorio = 0;
-
-    while(aux < 100){
-        b[aux] = 0;
-        aux++;
-    }
-
-    for(int j = 1; j < quant_Element; j++){
-            if(x[0][j] < 0){
-                somatorio = (x[0][j]*-1) + somatorio;
-            }else{
-                somatorio = x[0][j] + somatorio;
-            }
-    }
-
-    b[0] = somatorio / x[0][0];
-
-    for(int j = 1; j < quant_Element; j++){
-        for(int i = 0; i < j; i++){
-            if (i != j){
-                if(x[j][i] < 0){
-                    b[j] = (x[j][i]*-1) * b[j-1] + b[j];
+                if(x[i][j] < 0){
+                    somatorio = (x[i][j]*-1) + somatorio;
                 }else{
-                    b[j] = x[j][i]* b[j-1] + b[j];
+                    somatorio = x[i][j] + somatorio;
                 }
             }
         }
+        if(x[i][i] < 0){
+            if((x[i][i]* -1) > somatorio){
+                 //INDICA QUE A MATRIZ É ESTRITAMENTE DIAGONAL DOMINANTE
+            }else{
+                return -1;
+            }
+        }else{
+            if(x[i][i] > somatorio){
+                 //INDICA QUE A MATRIZ É ESTRITAMENTE DIAGONAL DOMINANTE
+            }else{
+                return -1;
+            }
+        }
+    }
+    return 1;
+}
 
-        b[j] = b[j]/x[j][j];
+int criterio_Sassenfeld(double x[100][100], int quant_Element){
+    double b[quant_Element];
 
-        if(b[j] > 1){
-            return -1; //INDICA QUE A MATRIZ NÃO OBEDECE O CRITERIO DE SASSENFELD
+    for(int t = 0; t < quant_Element; t++){
+        b[t] = 0;
+        if(t == 0){
+            for(int i = 1; i < quant_Element; i++){
+                b[t]=x[0][i] + b[t];
+            }
+            b[t] = b[t]/x[0][0];
+        }else{
+            for(int i = 0; i < quant_Element; i++){
+                if(i!= t){
+                    if(i < t){
+                        b[t]= b[i] * x[t][i];
+                    }else{
+                        b[t]= b[i] + x[t][i];
+                    }
+
+                    b[t]=b[t]/x[t][t];
+                }
+            }
         }
     }
 
-    return b[quant_Element]; //INDICA QUE A MATRIZ OBEDECE O CRITERIO DE SASSENFELD
+    double max=0;
+
+    for(int i = 0; i < quant_Element; i++){
+        if(b[i] > b[i+1]){
+            max = b[i];
+        }
+    }
+    if(max < 1){
+        return 1;
+    }
+        return -1;
 }
 
-float calc_Erro(float result_Atual, float result_Anterior){
-    float erro = (result_Atual-result_Anterior) / result_Atual;
+double calc_Erro(double result_Atual, double result_Anterior){
+    double erro = (result_Atual-result_Anterior) / result_Atual;
     if(erro < 0){
         erro = erro * -100;
     }else{
@@ -100,8 +107,8 @@ int main(){
 
     setlocale(LC_ALL,"");
 
-    int quant_Element, opc = 1, interacoes = 0;
-    float x[100][100], b[100], resultado[100], result_Anterior[100], erro;
+    int quant_Element,interacoes = 0;
+    double x[100][100], b[100], resultado[100], result_Anterior[100], erro,  opc = 1;
     printf("-------- ATENÇÃO --------\n\n");
     printf("A NOTAÇÃO X1, X2 , ...\n");
     printf("REPRESENTA AS VARIAVEIS DO SISTEMA\n\n");
@@ -110,27 +117,36 @@ int main(){
     system("pause");
     system("cls");
 
-    printf("IINSIRA A QUANTIDADE DE ELEMENTOS DA EQUAÇÃO (1 ATÉ 100): ");
+    printf("INSIRA A QUANTIDADE DE ELEMENTOS DA EQUAÇÃO (1 ATÉ 100): ");
     scanf("%d", &quant_Element);
 
     system("cls");
     printf("DÍGITE OS VALORES DO SISTEMA LINEAR\n\n");
+    //LÊ A EQUAÇÃO
     for(int i = 0; i < quant_Element; i++){
         printf("%dº EQUAÇÃO\n\n", i+1);
         for(int j = 0; j < quant_Element; j++){
             printf("X%d = ", j+1);
-            scanf("%f", &x[i][j]);
+            scanf("%lf", &x[i][j]);
+
+            if(i == j){
+                if(x[i][j] == 0){
+                    printf("FUNÇÃO CONTÉM UM ELEMENTO DA DIAGONAL = 0\nIMPOSSIVÉL ISOLAR X%d",j);
+                    return -1;
+                }
+            }
         }
 
         printf("B%d = ", i+1);
-        scanf("%f", &b[i]);
+        scanf("%lf", &b[i]);
 
         system("cls");
+        //IMPRIME A EQUAÇÃO NA TELA
         for(int k = 0; k <= i; k++){
             for(int q = 0; q < quant_Element; q++){
-                printf("%.2fX%d ", x[k][q], q+1);
+                printf("%.2lfX%d ", x[k][q], q+1);
             }
-            printf("= %.2f\n", b[k]);
+            printf("= %.2lf\n", b[k]);
         }
         printf("\n");
     }
@@ -144,7 +160,7 @@ int main(){
        }
 
     //CHAMA A FUNÇÃO TESTE DA MATRIZ ESTRITAMENTE DIAGONAL DOMINANTE
-    if(criterio_Linhas(x, quant_Element) == -1){
+    if( estri_Diag_Domin(x, quant_Element) == -1){
         printf("MATRIZ NÃO ESTRITAMENTE DIAGONAL DOMINANTE\n");
        }else{
         printf("MATRIZ ESTRITAMENTE DIAGONAL DOMINANTE\n");
@@ -169,12 +185,12 @@ int main(){
 
     system("cls");
     printf("PARAR QUANDO O ERRO ESTIVEM ABAIXO DE: ");
-    scanf("%f", &erro);
+    scanf("%lf", &erro);
 
     while(opc > erro){
 
     for(int i = 0; i < quant_Element; i++){
-        float somatorio = 0;
+        long double somatorio = 0;
         for(int j = 0; j < quant_Element; j++){
             if (i != j){
             somatorio = (-1*x[i][j])* resultado[j] + somatorio;
@@ -185,19 +201,19 @@ int main(){
     system("cls");
     for(int k = 0; k < quant_Element; k++){
             for(int q = 0; q < quant_Element; q++){
-                printf("%.2fX%d ", x[k][q], q+1);
+                printf("%.2lfX%d ", x[k][q], q+1);
             }
-            printf("= %.2f\n", b[k]);
+            printf("= %.2lf\n", b[k]);
         }
     printf("\n\n");
     for(int t = 0; t < quant_Element; t++){
         if(interacoes != 0){
-            printf("X%d = %.5f --> ERRO %.4f%%\n", t+1, resultado[t], calc_Erro(resultado[t], result_Anterior[t]));
+            printf("X%d = %.5lf --> ERRO %.4lf%%\n", t+1, resultado[t], calc_Erro(resultado[t], result_Anterior[t]));
             opc = calc_Erro(resultado[t], result_Anterior[t]);
             result_Anterior[t] = resultado[t];
         }else{
             result_Anterior[t] = resultado[t];
-            printf("X%d = %.5f\n", t+1, resultado[t]);
+            printf("X%d = %.5lf\n", t+1, resultado[t]);
         }
     }
     interacoes++;
